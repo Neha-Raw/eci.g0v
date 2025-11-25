6c3577ac74748f1f2f5986a5dc70c08b69e7317d
Neha-Raw <neharaao2002@gmail.com>
2025-11-25 13:46:50 +0000

    styling

--- applications/home/app/routes/index.js
+++ applications/home/app/routes/index.js
@@ -1,3 +1,14 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class IndexRoute extends Route {}
+export default class IndexRoute extends Route {
+  @service store;
+
+  async model() {
+    // Video fetch karo
+    let video = await this.store.findRecord('video');
+
+    // Return both video and modules to template
+    return { video, modules };
+  }
+}
