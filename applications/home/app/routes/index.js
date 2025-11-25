e35afeb3b74bcb079f9d73becfe7fe1294144482
Neha-Raw <neharaao2002@gmail.com>
2025-11-25 13:54:46 +0000

    add video

--- applications/home/app/routes/index.js
+++ applications/home/app/routes/index.js
@@ -1,14 +1,3 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
-export default class IndexRoute extends Route {
-  @service store;
-
-  async model() {
-    // Video fetch karo
-    let video = await this.store.findRecord('video');
-
-    // Return both video and modules to template
-    return { video, modules };
-  }
-}
+export default class IndexRoute extends Route {}
